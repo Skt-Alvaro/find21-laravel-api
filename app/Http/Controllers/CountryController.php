fc3b997aaff784d52a19c4ec99eea3d5ebdadec6
Alvaro <65323913+Skt-Alvaro@users.noreply.github.com>
2021-03-07 14:09:50 -0600

    test

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -17,17 +17,22 @@ class CountryController extends Controller
 
     public function store(Request $req)
     {
-        $country = new Country($req->input());
-
-        if ($req->file('image') == null) {
-            $req->image = "";
-        } else {
-            $country_file = $req->file('image')->store('public/images/countries');
-            $url = Storage::url($country_file);
-            $country->image = $url;
-        }
-        $country->save();
-
-        return response()->json($country, 200);
+        return $req;
     }
+
+    // public function store(Request $req)
+    // {
+    //     $country = new Country($req->input());
+
+    //     if ($req->file('image') == null) {
+    //         $req->image = "";
+    //     } else {
+    //         $country_file = $req->file('image')->store('public/images/countries');
+    //         $url = Storage::url($country_file);
+    //         $country->image = $url;
+    //     }
+    //     $country->save();
+
+    //     return response()->json($country, 200);
+    // }
 }
